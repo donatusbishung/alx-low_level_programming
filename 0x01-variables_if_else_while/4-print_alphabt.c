73743419ba0bb5d1fc46739bb2a3caa159e69a1e
donatusbishung <donaldbishung@yahoo.com>
2022-09-11 00:17:18 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main- print alphabet without q and a
+ *
+ * Description: program that prints alphabet without q and a
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for(i = 97; i < 123; i++)
+	{
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
