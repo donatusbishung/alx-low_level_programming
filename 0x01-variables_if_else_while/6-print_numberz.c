d03f15ec00d28df96a7b14758a79697abef9c71d
donatusbishung <donaldbishung@yahoo.com>
2022-09-11 01:07:11 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main- prints number in base 10
+ *
+ * Description: number in base 10 starting from 0
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int number;
+	
+	for (number = '0'; number <= '9'; number++)
+	{
+		putchar(number);
+	}
+	putchar('\n');
+
+	return (0);
+}
