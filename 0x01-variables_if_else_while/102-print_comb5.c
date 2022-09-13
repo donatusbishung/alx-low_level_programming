4ba74af44a069e59c40714fbed4c2e92af1ebf8e
donatusbishung <donaldbishung@yahoo.com>
2022-09-12 17:41:22 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,27 +11,27 @@
 
 int main(void)
 {
-	int i, e, j, h = 0;
+	int d1, d2, d3, d4 = 0;
 
-	for (i = '0'; i <= '9'; i++)
-	{
-	for (e = '0'; e <= '9'; e++)
-	{
-	for (j = '0'; j <= '9'; j++)
-	{
-	for (h = e + 1; h <= '9'; h++)
-	{
-		putchar(i);
-		putchar(e);
-		putchar(' ');
-		putchar(j);
-		putchar(h);
-		putchar(',');
-		putchar(' ');
-	}
-	}
-	}
-	}
+	for (d1 = '0'; d1 <= '9'; d1++)
+		{
+		for (d2 = '0'; d2 <= '9'; d2++)
+			{
+			for (d3 = '0'; d3 <= '9'; d3++)
+				{
+				for (d4 = d2 + 1; d4 <= '9'; d4++)
+					{
+						putchar(d1);
+						putchar(d2);
+						putchar(' ');
+						putchar(d3);
+						putchar(d4);
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
 	putchar('\n');
 
 	return (0);
