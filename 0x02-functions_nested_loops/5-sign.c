5f340ef5736779246fc50cace12f61557b863b5b
donatusbishung <donaldbishung@yahoo.com>
2022-09-13 23:38:49 -0700

    _isalpha

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isalpha - entry
+ *
+ * @c: character to be treated
+ *
+ * Return: 1 is character is alphabet, 0 otherwise.
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a'; c <= 'z'; c++) || (c >= 'A'; && c <= 'z'; c++))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
