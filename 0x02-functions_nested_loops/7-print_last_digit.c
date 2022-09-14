9f889368969bba97595889443ddc00dcbfe93bb8
donatusbishung <donaldbishung@yahoo.com>
2022-09-14 00:23:18 -0700

    print_last_digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - entry
+ *
+ * @number: integer to be printed
+ *
+ * Return: the last value of the last digit
+ */
+
+int print_last_digit(int number)
+{
+	if (number % 100)
+	{
+		return (number);
+	}
+	_putchar('\n');
+}
