5be0d7d3c90161080277d6ed23c386107c261174
donatusbishung <donaldbishung@yahoo.com>
2022-09-13 21:41:45 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,16 +3,16 @@
 /**
  * main- print alphabet letters
  *
- * Return: Always 0 
+ * Return: Always 0
  */
 
 void print_alphabet(void)
 {
 	int i;
+
 	for (i = 'a'; i <= 'z'; i++)
 	{
 		_putchar(i);
 	}
 	_putchar('\n');
-	
-}	
+}
