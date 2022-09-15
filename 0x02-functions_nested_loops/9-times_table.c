51e52de4f2e78dc090958a7005e4e176a849365c
donatusbishung <donaldbishung@yahoo.com>
2022-09-14 19:32:50 -0700

    times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,12 +15,12 @@ void times_table(void)
 		_putchar('0');
 		_putchar(',');
 		_putchar(' ');
-		for (c 1; c <= 9; c++)
+		for (c = 1; c <= 9; c++)
 		{
 			d = (r * c);
 			if ((d / 10) > 0)
 			{
-				_putchar((d / 10) '0');
+				_putchar((d / 10) + '0');
 			}
 			else
 			{
