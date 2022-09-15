e83f13ca5127f2bb124c726606bdc6b5008c7bab
donatusbishung <donaldbishung@yahoo.com>
2022-09-14 19:57:12 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - entry
+ *
+ * @a: first integer
+ *
+ * @b: second integer
+ *
+ * Return: addition of a and b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
