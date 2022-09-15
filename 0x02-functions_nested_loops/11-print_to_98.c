1f01b95e7f82676682f3011bc084e22a9e51606a
donatusbishung <donaldbishung@yahoo.com>
2022-09-14 20:10:40 -0700

    print_to_98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_to_98 - entry
+ *
+ * @n: integer input
+ *
+ * Return: void
+ */
+
+void print_to_98(int n)
+{
+	while (n != 98)
+	{
+		printf("%d", n);
+		if (n > 98)
+			n--;
+		else
+			n++;
+	}
+	_putchar("98\n");
+}
