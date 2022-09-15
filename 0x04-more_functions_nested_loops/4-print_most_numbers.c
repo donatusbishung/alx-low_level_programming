9014a697e8213fe297588ad1f7cf87615d8788f6
donatusbishung <donaldbishung@yahoo.com>
2022-09-15 16:58:10 -0700

    4-print_most_numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - entry
+ *
+ * Return: do not print 2 and 4
+ */
+
+void print_most_numbers(void)
+{
+	int item;
+
+	for (item = 0; item <= 9; item++)
+	{
+		if (item == 2 || item == 4)
+			continue;
+	else
+		_putchar(item);
+	}
+	_putchar('\n');
+}
