25e4c8d462da64f9e262647831e42caff373f506
donatusbishung <donaldbishung@yahoo.com>
2022-09-14 21:46:45 -0700

    naturals

---
+++ 0x02-functions_nested_loops/101-naturals.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ *
+ * Description: multiplies between 0 and 1024
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a < 1024; a++)
+	{
+		if ((a % 3) == 0 || (a % 5) == 0)
+			b += a;
+	}
+	printf("%d", b);
+
+	return (0);
+}
