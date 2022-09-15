8c8aa9f59b41b071ee705aeffbbe282c81cfa211
donatusbishung <donaldbishung@yahoo.com>
2022-09-14 18:46:47 -0700

    times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,10 +8,13 @@
 
 void times_table(void)
 {
-	int start, stop;
+	int start = 0;
+	int stop = 0;
 
 	for (start = 0; start <= 9; start++)
 	{
+		for (stop = 0; stop <= 9; stop++)
+		{
 		int result = start * stop;
 
 		if (start == 0)
@@ -20,6 +23,7 @@ void times_table(void)
 			printf(", %i", result);
 		else
 		printf(",%i", result);
+	}
 	}
 	_putchar('\n');
 }
