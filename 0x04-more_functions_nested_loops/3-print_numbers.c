dacb956500bd8baca60d39fc260ed400492c5e4b
donatusbishung <donaldbishung@yahoo.com>
2022-09-15 16:38:17 -0700

    3-print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - entry
+ *
+ * Return: no return
+ */
+
+void print_numbers(void)
+{
+	int ch;
+
+	for (ch = 0; ch <= 9; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
