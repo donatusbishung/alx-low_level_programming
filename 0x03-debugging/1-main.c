938fabcc7508f1e46085e3e1fb34be33f64472be
donatusbishung <donaldbishung@yahoo.com>
2022-09-16 19:03:04 -0700

    debugging 1-main

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - causes an infinite loop
+ * Return: o
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incomimg :(\n");
+
+	i = 0;
+
+	/*while (i < 10)*/
+	/*{*/
+	/*putchar(i);*/
+	/*}*/
+
+	printf("Infinite loop avoided! \\0/\n");
+
+	return (0);
+}
