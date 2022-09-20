309b5d4a6b9c64992a884d95324758a88a41b04c
donatusbishung <donaldbishung@yahoo.com>
2022-09-19 20:37:36 -0700

    converts a string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,29 +9,29 @@
 
 int _atoi(char *s)
 {
-	unsigned int number = 0, size = 0, oi = 0, pn = 1, m = 1, i;
+	unsigned int number = 0, a = 0, d = 0, f = 1, g = 1, i;
 
 	while (*(s + number) != '\0')
 	{
-		if (size > 0 && (*(s + number) < '0' || *(s + number) > '9'))
+		if (a > 0 && (*(s + number) < '0' || *(s + number) > '9'))
 			break;
 
 		if (*(s + number) == '-')
-			pn *= -1;
+			f *= -1;
 
 		if ((*(s + number) >= '0') && (*(s + number) <= '9'))
 		{
-			if (size > 0)
-				m *= 10;
-			size++;
+			if (a > 0)
+				g *= 10;
+			a++;
 		}
 		number++;
 	}
 
-	for (i = number - size; i < number; i++)
+	for (i = number - a; i < number; i++)
 	{
-		oi = oi + ((*(s + i) - 48) * m);
-		m /= 10;
+		d = d + ((*(s + i) - 48) * g);
+		g /= 10;
 	}
-	return (oi * pn);
+	return (d * f);
 }
