b4f070d0dcbbbcc9820e0626cf447281af06e383
donatusbishung <donaldbishung@yahoo.com>
2022-09-19 19:59:32 -0700

    prints element of a array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -15,9 +15,9 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-	if (i == (n - 1))
+		if (i == (n - 1))
 			printf("%d\n", a[i]);
-	else
-		printf("%i, ", a[i]);
+		else
+			printf("%i, ", a[i]);
 	}
 }
