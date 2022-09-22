2914d5e8fbb68cfedbb103c52edf64ec68bba551
donatusbishung <donaldbishung@yahoo.com>
2022-09-22 05:49:32 -0700

    Updated main and putchar

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _putchar - writes the char c to stdout
+ *
+ * @c: character to print
+ *
+ * Return: On success 1. and on erro -1.
+ */
+
+int _putchar(char c)
+{
+	return(write(1, &c, 1));
+}
