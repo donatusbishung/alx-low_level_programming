8f90430728e78823146bebbb1b737368059decfd
donatusbishung <donaldbishung@yahoo.com>
2022-09-22 15:51:54 -0700

    capitalizing all words of a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @str: string
+ *
+ * Return: 0
+ */
+
+char *cap_string(char *str)
+{
+	int len = 0, i;
+	int my_char[] = {9, 10, 32, 33, 34, 40, 41, 44, 46, 59, 63, 123, 125};
+
+	if (*(str + len) >= 97 && *(str + len) <= 122)
+		*(str + len) = *(str + len) - 32;
+	len++;
+	while (*(str + len) != '\0')
+	{
+		for (i = 0; i < 13; i++)
+		{
+			if (*(str + len) == my_char[i])
+			{
+				if ((*(str + (len + 1)) >= 97) && (*(str + (len + 1)) <= 122))
+					*(str + (len + 1)) = *(str + (len + 1)) - 32;
+				break;
+			}
+		}
+		len++;
+	}
+	return (str);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,5 +10,11 @@ char *_strncpy(char *dest, char *src, int n);
 char *_strncat(char *dest, char *src, int n);
 char *_strcat(char *dest, char *src);
 int _putchar(char c);
+char *cap_string(char *str);
+char *leet(char *str);
+void print_number(int n);
+char *rot13(char *str);
+void print_buffer(char *b, int size);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 #endif
