63f2b8ddef635d6df4e97f4dfbbff16c2b1b441a
donatusbishung <donaldbishung@yahoo.com>
2022-09-26 19:11:56 -0700

    pointer to pointer

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - sets value of pointer to a char
+ * @s: double pointer
+ * @to: pointing to
+ * Return: 0
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
