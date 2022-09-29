cfa3e573376e86b99164aab7d18bac7ea8e27555
donatusbishung <donaldbishung@yahoo.com>
2022-09-28 18:49:59 -0700

    prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * prime - checking for prime number
+ * @n: number
+ * @d: divisor
+ * Return: 1 if prime number, o if not
+ */
+
+int prime(int n, int d)
+{
+	if (n == d)
+		return (1);
+	else if (n % d == 0)
+		return (0);
+	else
+		return (prime(n, d + 1));
+}
+/**
+ * is_prime_number - returns 1 if input is a prime number
+ * @n: number
+ * Return: 1 if prime number, 0 if otherwise
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 0)
+		return (0);
+	else if (n == 1)
+		return (0);
+	else
+		return (prime(n, 2));
+}
