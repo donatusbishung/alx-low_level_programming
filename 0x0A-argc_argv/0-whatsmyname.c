e87262a468d574f7b0cefd25f4752f99f52fdb75
donatusbishung <donaldbishung@yahoo.com>
2022-10-01 09:41:18 -0700

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints name
+ * @argc: number of command line argument
+ * @argv: program command line argument
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
