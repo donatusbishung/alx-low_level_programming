58c81eaa87e4f146c511e1130ce61727d8e5a391
donatusbishung <donaldbishung@yahoo.com>
2022-10-04 18:23:49 -0700

    Second commit on Malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strup - program with a function that returns a pointer to a
+ * newly allocated space in memory which contains a string given as
+ * a parameter.
+ * @str: string
+ * Return: returns NULL if insufficient space is available
+ */
+
+char *_strup(char *str)
+{
+	int i = 0, j = 0;
+	char *c;
+
+	if (str == 0)
+		return (NULL);
+	while (str[i] != '\0')
+		i++;
+	c = malloc(sizeof(char) * (i + 1));
+	if (c == 0)
+		return (NULL);
+	for (; j <= i; j++)
+		c[j] = str[j];
+	return (c);
+}
