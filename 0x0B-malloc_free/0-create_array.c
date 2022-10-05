c9d4eb4a9bedb77b175de096562fc76471e7f038
donatusbishung <donaldbishung@yahoo.com>
2022-10-04 17:51:59 -0700

    My first memory allocation commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib>
+
+
+/**
+ * create_array - prograam with a function that
+ * creates an array of char, and innitializes it
+ * with a specific char.
+ * @size: integer size
+ * @c: string character
+ *
+ * Return: A pointer to the array or Null if it fails.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int = i;
+	char *su;
+
+	if (size == Null)
+		return (Null);
+	su = malloc(size * sizeof(char(c)));
+	if (su == Null)
+		return (Null);
+	for (i = 0; i < size; i++)
+		su[i] = c;
+}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+char *create_array(unsigned in size, char c);
+
+#endif /* MAIN_H */
