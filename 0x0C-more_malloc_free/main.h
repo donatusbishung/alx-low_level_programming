c3784cd969f9638885d1d1f5521332119ae29b75
donatusbishung <donaldbishung@yahoo.com>
2022-10-08 06:03:06 +0100

    kkk

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -9,5 +9,6 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void _print(char *str, int l);
+int _putchar(char c);
 
 #endif /* MAIN_H */
