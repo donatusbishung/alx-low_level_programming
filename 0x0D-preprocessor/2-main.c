598eb1d1d393662d137ce0f262a3163574ffd784
donatusbishung <donaldbishung@yahoo.com>
2022-10-11 03:19:50 +0100

    Name of file then \n

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file it was compiled
+ * from, followed by \n
+ *
+ * Return: Always 0
+ */
+
+void main(void)
+{
+	printf("%s\n" _File_);
+	return (0);
+}
