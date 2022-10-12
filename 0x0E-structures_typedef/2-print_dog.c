3d4b1eb97d09e50ea56769a37a4f6ca651603e0b
donatusbishung <donaldbishung@yahoo.com>
2022-10-12 04:01:27 +0100

    Printing the initialized variables of struct dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,16 +14,17 @@ void print_dog(struct dog *d)
 	if (d)
 	{
 		if ((*d).name != NULL)
-			printf("Name: (nil)\n");
-		else
 			printf("Name: %s\n", d->owner);
+		else
+
+			printf("Name: (nil)\n");
 		if ((*d).age != 0)
 			printf("Age: %.4f\n", d->age);
 		else
 			printf("Age: (nil)\n");
 		if ((*d).owner != NULL)
-			printf("Owner: (nil)\n");
-		else
 			printf("Owner: %s\n", d->owner);
+		else
+			printf("Owner: (nil)\n");
 	}
 }
