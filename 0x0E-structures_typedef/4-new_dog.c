e1f3965d516da31a28c977affe1a220764a365ce
donatusbishung <donaldbishung@yahoo.com>
2022-10-12 04:32:21 +0100

    initializing new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,7 +12,7 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	unsigned int nl, ol, i;
+	int nl, ol, i;
 	dog_t *dog;
 
 	if (name == NULL || owner == NULL)
@@ -23,26 +23,26 @@ dog_t *new_dog(char *name, float age, char *owner)
 	for (nl = 0; name[nl]; nl++)
 		;
 	nl++;
-	(*dog).name = malloc(nl * sizeof(char));
-	if ((*dog).name == NULL)
+	dog->name = malloc(nl * sizeof(char));
+	if (dog->name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
 	for (i = 0; i < nl; i++)
-		(*dog).name[i] = name[i];
-	(*dog).age = age;
+		dog->name[i] = name[i];
+	dog->age = age;
 	for (ol = 0; owner[ol]; ol++)
 		;
 	ol++;
-	(*dog).owner = malloc(ol * sizeof(char));
-	if ((*dog).owner == NULL)
+	dog->owner = malloc(ol * sizeof(char));
+	if (dog->owner == NULL)
 	{
-		free((*dog).name);
+		free(dog->name);
 		free(dog);
 		return (NULL);
 	}
 	for (i = 0; i < ol; i++)
-		(*dog).owner[i] = owner[i];
+		dog->owner[i] = owner[i];
 	return (dog);
 }
