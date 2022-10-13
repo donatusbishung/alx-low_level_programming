dda63bbd31b452a5c041861d36b117cf626e1174
donatusbishung <donaldbishung@yahoo.com>
2022-10-13 11:57:44 +0100

    Math Operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -20,15 +20,24 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
+	if (argv[2][1])
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
 	f = get_op_func(arg[2]);
+
 	if (f == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
 	c = f(a, b);
+
 	printf("%d\n", c);
 	return (0);
 }
