d5e0c06dd897a680faa09b8cd97762598bae282f
donatusbishung <donaldbishung@gmail.com>
2022-10-26 02:30:54 +0100

    prototype and print all

--- 0-print_listint.c
+++ 0-print_listint.c
@@ -8,13 +8,13 @@
 
 size_t print_listint(const listint_t *h)
 {
-	int n;
+	int value = 0;
 
 	while (h != NULL)
 	{
-		printf("%d\n", h->n);
-		h = h->;
-		n++;
+		printf("%d\n", h->value);
+		h = h->next;
+		value++;
 	}
 	return (n);
 }
