cb2a1986a82f091e68933847fcedacd2522fa4b5
donatusbishung <donaldbishung@yahoo.com>
2022-10-27 05:56:26 +0100

    changes

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -28,5 +28,6 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 void free_listint2(listint_t **head);
 void free_listint(listint_t *head);
 int _putchar(char c);
+listint_t *find_listint_loop(listint_t *head);
 
 #endif /* LISTS_H */
