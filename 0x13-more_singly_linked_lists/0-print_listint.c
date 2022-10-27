707e66964d2615c372bdb453f505b2cdbed46bc8
donatusbishung <donaldbishung@yahoo.com>
2022-10-27 05:13:35 +0100

    fix

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,13 +8,24 @@
 
 size_t print_listint(const listint_t *h)
 {
-	int tmp = 0;
+	size_t tmp = 0;
+	listint_t *node;
 
-	while (h)
+	if (h == NULL)
+		return (tmp);
+
+	if (h->n != 0)
 	{
 		printf("%d\n", h->n);
-		h = h->next;
 		tmp++;
 	}
+
+	if (h->next == NULL)
+		return (tmp);
+	node = h->next;
+
+	for (; node != NULL; tmp++, node = node->next)
+		printf("%d\n", node->n);
+
 	return (tmp);
 }
