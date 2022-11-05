ef0688d2aa75e9852e965c1dbf8c8da69a280121
donatusbishung <donaldbishung@yahoo.com>
2022-11-05 02:21:39 +0100

    reading and writing into a file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,25 +10,25 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int sd, fr, prnt;
-    char *c = (char *)malloc(sizeof(char) * letters);
+int sd, fr, prnt;
+char *c = (char *)malloc(sizeof(char) * letters);
 
-    if (filename == NULL)
-	return (0);
+if (filename == NULL)
+return (0);
 
-    sd = open(filename, O_RDONLY);
+sd = open(filename, O_RDONLY);
 
-    if (sd < 0)
-	return (0);
+if (sd < 0)
+return (0);
 
-    if (c == NULL)
-	return (0);
+if (c == NULL)
+return (0);
 
-    fr = read(fd, c, letters);
-    prnt = write(STDOUT_FILENO, c, fr);
+fr = read(fd, c, letters);
+prnt = write(STDOUT_FILENO, c, fr);
 
-    close(sd);
-    free(c);
+close(sd);
+free(c);
 
-    return (prnt);
+return (prnt);
 }
