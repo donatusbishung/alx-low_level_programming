cd5b9da8a820947af59ea26c447556598c4ad737
donatusbishung <donaldbishung@yahoo.com>
2022-11-05 04:59:36 +0100

    text to file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,36 +1,37 @@
 #include "main.h"
 
 /**
- * read_textfile - program reads a txt file and prints it to the stdout
- * @filename: to be read from
- * @letters: no. of letters to read and print
- * Return: reads a text file and prints it to the POSIX
- * stdout
+ * read_textfile - program reads a text file and print it to POSIX stdout
+ * @letters: the number of letters the function should print.
+ * @filename: a pointer to the file name of the file
+ *
+ * Return: if the func. fails of the filename is NULL - 0.
+ * otherwise the number f bytes the func. can read of print.
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-ssize_t o, r, w;
-char *buffer;
+	ssize_t o, r, w;
+	char *buffer;
 
-if (filename == NULL)
-return (0);
+	if (filename == NULL)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
 
-buffer = malloc(sizeof(char) * letters);
-if (buffer == NULL)
-return (0);
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
 
-o = open(filename, O_RDONLY);
-r = read(o, buffer, letters);
-w = write(STDOUT_FILENO, buffer, r);
+	if (o == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(buffer);
+		return (0);
+	}
 
-if (o == -1 || r == -1 || w != r)
-{
-free(buffer);
-return (0);
-}
-free(buffer);
-close(o);
+	free(buffer);
+	close(o);
 
-return (w);
+	return (w);
 }
