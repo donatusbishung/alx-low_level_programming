44ea0d54bdfd25536c8cd38b7a5c0c88cf0280a7
donatusbishung <donaldbishung@yahoo.com>
2023-10-24 22:53:28 +0100

    printing list edit

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,7 +3,7 @@
 
 /**
  * print_listint - prints all the elements of a listint_t list.
- * @h: head of linklist node
+ * @h: head of linked list node
  * Return: the number of nodes
  */
 size_t print_listint(const listint_t *h)
